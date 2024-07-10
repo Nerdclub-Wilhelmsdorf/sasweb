964576db8f320d796035ea3f17c8094e7dcb742d
mail@lenblum.de <mail@lenblum.de>
2024-07-10 08:33:49 +0200

    fixing light mode

--- src/lib/pay.svelte
+++ src/lib/pay.svelte
@@ -77,7 +77,7 @@
                 SenderAccount.set("");
             }}
         >
-            <Qr_code class="w-6 h-6 text-secondary-700" style="outline: none; box-shadow: none; border: none;" />
+            <Qr_code class="w-6 h-6 text-secondary-700 text-white" style="outline: none; box-shadow: none; border: none;" />
         </Button>
 
         <Checkbox
@@ -112,7 +112,7 @@
                 SenderAccount.set("");
             }}
         >
-            <Qr_code class="w-6 h-6 text-secondary-700" style="outline: none; box-shadow: none; border: none;" />
+            <Qr_code class="w-6 h-6 text-secondary-700  text-white" style="outline: none; box-shadow: none; border: none;" />
         </Button>
 
         <Checkbox
